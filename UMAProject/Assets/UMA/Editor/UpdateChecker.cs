44388fead8d13b37866820901b301a2573075045
Victor Hagsand <victor_hagsand@hotmail.com>
2017-01-26 22:59:51 -0500

    Show updater at startup if update is availible.

---
+++ UMAProject/Assets/UMA/Editor/UpdateChecker.cs
@@ -0,0 +1,40 @@
+﻿using UnityEditor;
+using UnityEngine;
+using kode80.Versioning;
+
+[InitializeOnLoad]
+public class UpdateChecker
+{
+    static UpdateChecker()
+    {
+        EditorApplication.update += RunOnce;
+    }
+    private static void RunOnce()
+    {
+        if(EditorPrefs.GetFloat("UMA_EditorTime") > EditorApplication.timeSinceStartup)
+        {
+            EditorPrefs.SetFloat("UMA_EditorTime", (float)EditorApplication.timeSinceStartup);
+            EditorApplication.update -= RunOnce;
+
+            AssetUpdater.Instance.Refresh();
+
+            AssetUpdater.Instance.remoteVersionDownloadFinished += RemoteVersionDownloadFinished;
+            AssetUpdater.Instance.remoteVersionDownloadFailed += RemoteVersionDownloadFailed;
+        }
+    }
+
+    private static void RemoteVersionDownloadFinished( AssetUpdater updater, int assetIndex)
+    {
+        AssetVersion remote = updater.GetRemoteVersion(0);
+        AssetVersion local = updater.GetLocalVersion(0);
+        if(remote != local)
+        {
+            AssetUpdateWindow.Init();
+        }
+    }
+
+    private static void RemoteVersionDownloadFailed( AssetUpdater updater, int assetIndex)
+    {
+        Debug.Log("Failed to get remote UMA version.");
+    }
+}
